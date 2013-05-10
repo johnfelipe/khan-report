16e843613fd71014cd0bb4b398e2f36da75a8e8a
Mikulas <rullaf@gmail.com>
2013-05-10 15:25:04 +0200

    remove videos that dropped below 100 %

--- cron.php
+++ cron.php
@@ -12,7 +12,7 @@ define('TEMP_FILE', __DIR__ . '/status.tgz');
 define('DATA_DIR', __DIR__ . '/data');
 
 $file_last = DATA_DIR . '/' . date('Y-m-d') . '.xls';
-//*
+/*
 echo "downloading...\n";
 file_put_contents(TEMP_FILE, file_get_contents(URL));
 echo "done\n";
@@ -38,6 +38,7 @@ echo "done\n";
 echo "read new xls\n";
 
 $reader = new Spreadsheet_Excel_Reader($file_last);
+echo "loaded\n";
 
 $keys = [];
 $rowcount = $reader->rowcount(1);
@@ -45,18 +46,32 @@ $newdata = [];
 
 for ($row = 2; $row < $rowcount; ++$row) {
 	$done = $reader->val($row, 5, 1) === 100;
+	$language = $reader->val($row, 4, 1);
+
+	if (!isset($newdata[$language])) {
+		// we need to iterate over every language later
+		$newdata[$language] = [];
+	}
 	if (!$done) {
 		continue;
 	}
 
 //	$video = $reader->val($row, 2, 1);
 	$youtube_id = $reader->val($row, 3, 1);
-	$language = $reader->val($row, 4, 1);
 
 	$newdata[$language][] = $youtube_id;
 }
 unset($reader);
 echo "done\n";
+
+var_dump("test, removing " . $newdata['Greek'][0]);
+var_dump(count($newdata['Greek']));
+do {
+	$k = array_search('9p10aMu1Glo', $newdata['Greek']);
+	unset($newdata['Greek'][$k]);
+} while ($k !== FALSE);
+
+var_dump(count($newdata['Greek']));
 
 echo "saving new data to files\n";
 foreach ($newdata as $language => $data) {
@@ -67,11 +82,22 @@ foreach ($newdata as $language => $data) {
 	$diff = array_diff($data, $old);
 	echo "updating $language (" . count($diff) . ")\n";
 
+	// videos that were at 100% but dropped lower (because English translation has been extended)
+	$removed = array_unique(array_diff($old, $data));
+
 	$output = '';
+	foreach (explode("\n", file_get_contents($file)) as $line) {
+		list($id) = explode(" ", $line);
+		if (!in_array($id, $removed)) {
+			$output .= "$line\n";
+		}
+	}
+
 	foreach ($diff as $youtube_id) {
 		$output .= "$youtube_id " . date('Y-m-d h:i') . "\n";
 	}
-	file_put_contents($file, $output, FILE_APPEND);
+
+	file_put_contents($file, $output);
 }
 echo "done\n";
 
