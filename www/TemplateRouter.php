2e395b4870a5c269bc1553cd71fd43bdc6b6e1e3
Mikulas <rullaf@gmail.com>
2013-05-10 20:45:43 +0200

    add root gitignore

---
+++ www/TemplateRouter.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ */
+
+use Nette\Application\Routers,
+	Nette\Latte;
+
+
+
+/**
+ * Micro-framework router for templates using {url} macro.
+ *
+ * @author     David Grudl
+ */
+class TemplateRouter extends Routers\RouteList
+{
+
+	public function __construct($path, $cachePath)
+	{
+		if (is_file($cacheFile = $cachePath . '/routes.php')) {
+			$routes = require $cacheFile;
+		} else {
+			$routes = array();
+			foreach (Nette\Utils\Finder::findFiles('*.latte')->from($path) as $file) {
+				$latte = new Latte\Engine;
+				$macroSet = new Latte\Macros\MacroSet($latte->compiler);
+				$macroSet->addMacro('url', function($node) use (&$routes, $file) {
+					$routes[$node->args] = (string) $file;
+				});
+				$latte->__invoke(file_get_contents($file));
+			}
+			file_put_contents($cacheFile, '<?php return ' . var_export($routes, TRUE) . ';');
+		}
+
+		foreach ($routes as $mask => $file) {
+			$this[] = new Routers\Route($mask, function($presenter) use ($file) {
+				return $presenter->createTemplate(NULL, function() {
+					$latte = new Nette\Latte\Engine;
+					$macroSet = new Latte\Macros\MacroSet($latte->compiler);
+					$macroSet->addMacro('url', '');
+					return $latte;
+				})->setFile($file);
+			});
+		}
+	}
+
+}
--- www/index.php
+++ www/index.php
@@ -1,209 +1,39 @@
 <?php
-$languages = ['ab' => 'Abkhazian', 'gv' => 'Manx', 'gu' => 'Gujarati', 'nan' => 'Hokkien', 'fr-ca' => 'French, Canadian', 'sco' => 'Scots', 'mni' => 'Manipuri', 'gd' => 'Scottish Gaelic', 'pt-br' => 'Portuguese, Brazilian', 'ga' => 'Irish', 'gn' => 'Guaran', 'gl' => 'Galician', 'oji' => 'Ojibwe', 'tsn' => 'Tswana', 'lg' => 'Ganda', 'lb' => 'Luxembourgish', 'ty' => 'Tahitian', 'es-ar' => 'Spanish, Argentinian', 'tt' => 'Tartar', 'tr' => 'Turkish', 'ts' => 'Tsonga', 'li' => 'Limburgish', 'lv' => 'Latvian', 'to' => 'Tonga', 'lt' => 'Lithuanian', 'lu' => 'Luba-Katagana', 'tk' => 'Turkmen', 'th' => 'Thai', 'fy-nl' => 'Frisian', 'tg' => 'Tajik', 'lkt' => 'Lakota', 'te' => 'Telugu', 'hau' => 'Hausa', 'fil' => 'Filipino', 'ta' => 'Tamil', 'yi' => 'Yiddish', 'amh' => 'Amharic', 'ceb' => 'Cebuano', 'mos' => 'Mossi', 'meta-geo' => 'Metadata: Geo', 'de' => 'German', 'da' => 'Danish', 'moh' => 'Mohawk', 'dz' => 'Dzongkha', 'dv' => 'Divehi', 'zh-hk' => 'Chinese, Traditional (Hong Kong)', 'ibo' => 'Igbo', 'pam' => 'Kapampangan', 'mnk' => 'Mandinka', 'pan' => 'Eastern Punjabi', 'bam' => 'Bambara', 'el' => 'Greek', 'eo' => 'Esperanto', 'en' => 'English', 'zh' => 'Chinese, Yue', 'ee' => 'Ewe', 'xho' => 'Xhosa', 'za' => 'Zhuang, Chuang', 'meta-wiki' => 'Metadata: Wikipedia', 'eu' => 'Basque', 'et' => 'Estonian', 'tet' => 'Tetum', 'es' => 'Spanish', 'lua' => 'Luba-Kasai', 'en-gb' => 'English, British', 'ru' => 'Russian', 'ful' => 'Fula', 'ff' => 'Fulah', 'luy' => 'Luhya', 'ast' => 'Asturian', 'ry' => 'Rusyn', 'wol' => 'Wolof', 'iro' => 'Iroquoian languages', 'rm' => 'Romansh', 'ro' => 'Romanian', 'bn' => 'Bengali', 'luo' => 'Luo', 'be' => 'Belarusian', 'bg' => 'Bulgarian', 'run' => 'Rundi', 'ba' => 'Bashkir', 'wa' => 'Walloon', 'hai' => 'Haida', 'zh-sg' => 'Chinese, Simplified (Singaporean)', 'jv' => 'Javanese', 'bo' => 'Tibetan', 'bh' => 'Bihari', 'bi' => 'Bislama', 'orm' => 'Oromo', 'meta-tw' => 'Metadata: Twitter', 'ase' => 'American Sign Language', 'bs' => 'Bosnian', 'rup' => 'Macedo', 'ss' => 'Swati', 'ja' => 'Japanese', 'umb' => 'Umbundu', 'fa' => 'Persian', 'ilo' => 'Ilocano', 'la' => 'Latin', 'oc' => 'Occitan', 'srp' => 'Montenegrin', 'tw' => 'Twi', 'que' => 'Quechua', 'aka' => 'Akan', 'tlh' => 'Klingon', 'os' => 'Ossetian, Ossetic', 'zh-cn' => 'Chinese, Simplified', 'zul' => 'Zulu', 'kin' => 'Rwandi', 'ch' => 'Chamorro', 'co' => 'Corsican', 'br' => 'Breton', 'nso' => 'Northern Sotho', 'ca' => 'Catalan', 'som' => 'Somali', 'ce' => 'Chechen', 'skx' => 'Seko Padang', 'cy' => 'Welsh', 'sr-latn' => 'Serbian, Latin', 'sot' => 'Sotho', 'cs' => 'Czech', 'cr' => 'Cree', 'es-mx' => 'Spanish, Mexican', 'cv' => 'Chuvash', 'cu' => 'Church Slavic', 'ps' => 'Pashto', 'or' => 'Oriya', 'pt' => 'Portuguese', 'kon' => 'Kongo', 'swa' => 'Swahili', 'zh-tw' => 'Chinese, Traditional', 'tl' => 'Tagalog', 'cho' => 'Choctaw', 'pi' => 'Pali', 'pl' => 'Polish', 'hz' => 'Herero', 'hy' => 'Armenian', 'hr' => 'Croatian', 'kn' => 'Kannada', 'ht' => 'Creole, Haitian', 'hu' => 'Hungarian', 'hi' => 'Hindi', 'hup' => 'Hupa', 'ho' => 'Hiri Motu', 'pnb' => 'Western Punjabi', 'bug' => 'Buginese', 'he' => 'Hebrew', 'haz' => 'Hazaragi', 'es-ni' => 'Spanish, Nicaraguan', 'mlg' => 'Malagasy', 'uz' => 'Uzbek', 'ml' => 'Malayalam', 'mo' => 'Moldavian, Moldovan', 'mn' => 'Mongolian', 'mi' => 'Maori', 'mh' => 'Marshallese', 'mk' => 'Macedonian', 'ur' => 'Urdu', 'mt' => 'Maltese', 'ms' => 'Malay', 'mr' => 'Marathi', 'ber' => 'Berber', 'ug' => 'Uyghur', 'my' => 'Burmese', 'aa' => 'Afar', 'ab' => 'Abkhazian', 'ae' => 'Avestan', 've' => 'Venda', 'af' => 'Afrikaans', 'vi' => 'Vietnamese', 'is' => 'Icelandic', 'iu' => 'Inuktitut', 'it' => 'Italian', 'an' => 'Aragonese', 'ii' => 'Sichuan Yi', 'ia' => 'Interlingua', 'ik' => 'Inupia', 'ar' => 'Arabic', 'inh' => 'Ingush', 'km' => 'Khmer', 'prs' => 'Dari', 'av' => 'Avaric', 'ay' => 'Aymara', 'az' => 'Azerbaijani', 'ie' => 'Interlingue', 'id' => 'Indonesian', 'as' => 'Assamese', 'pap' => 'Papiamento', 'sk' => 'Slovak', 'sr' => 'Serbian', 'nl' => 'Dutch', 'lo' => 'Lao', 'nn' => 'Norwegian Nynorsk', 'na' => 'Naurunan', 'nb' => 'Norwegian Bokmal', 'nd' => 'North Ndebele', 'ne' => 'Nepali', 'tir' => 'Tigrinya', 'ng' => 'Ndonga', 'efi' => 'Efik', 'vo' => 'Volapuk', 'bnt' => 'Ibibio', 'nr' => 'Southern Ndebele', 'nya' => 'Chewa', 'yor' => 'Yoruba', 'nv' => 'Navajo', 'fr' => 'French', 'sm' => 'Samoan', 'io' => 'Ido', 'sna' => 'Shona', 'kik' => 'Gikuyu', 'kar' => 'Karen', 'mad' => 'Madurese', 'kau' => 'Kanuri', 'fi' => 'Finnish', 'fj' => 'Fijian', 'sa' => 'Sanskrit', 'uk' => 'Ukrainian', 'fo' => 'Faroese', 'ka' => 'Georgian', 'arq' => 'Algerian Arabic', 'kk' => 'Kazakh', 'kj' => 'Kuanyama, Kwanyama', 'sq' => 'Albanian', 'ko' => 'Korean', 'sv' => 'Swedish', 'su' => 'Sundanese', 'kl' => 'Greenlandic', 'ks' => 'Kashmiri', 'si' => 'Sinhala', 'sh' => 'Serbo-Croatian', 'kw' => 'Cornish', 'kv' => 'Komi', 'ku' => 'Kurdish', 'sl' => 'Slovenian', 'sc' => 'Sardinian', 'ky' => 'Kyrgyz', 'sg' => 'Sango', 'lin' => 'Lingala', 'se' => 'Northern Sami', 'sd' => 'Sindhi'];
-foreach ($languages as &$l) {
-  $l = str_replace(':', '-', strToLower($l));
-  $l = str_replace(',', '-', $l);
-  $l = str_replace(' ', '', $l);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/TemplateRouter.php';
+$languages = require __DIR__ . '/languages.php';
+
+
+$configurator = new Nette\Configurator;
+$configurator->enableDebugger(__DIR__ . '/../log');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$container = $configurator->createContainer();
+
+$container->addService('router', new TemplateRouter('templates', __DIR__ . '/../temp'));
+$container->application->run();
+
+function isDaemonRunning()
+{
+  return `ps aug | grep daemon.php | wc -l` >= 2;
 }
 
-if (isset($_GET['language'])) {
-  $lang = array_search($_GET['language'], $languages);
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>New translated content on Khan Academy</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+function getTranslations($language)
+{
+  $handle = fopen(__DIR__ . "/../translations.dat", "r");
+  $data = [];
+  while (!feof($handle)) {
+    $line = fgets($handle);
+    if (!$line)
+      continue;
 
-    <!-- Le styles -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/chosen.css" rel="stylesheet">
-    <style type="text/css">
-      .hero-unit h1 {font-size: 20pt;}
-      /* zeroclipboard */
-      [data-clipboard-text] {width: 16px; height: 16px; border: none; text-align:center; background: url('/img/copy.png') no-repeat; opacity: .6}
-      [data-clipboard-text].zeroclipboard-is-hover {opacity: 1}
-      [data-clipboard-text].zeroclipboard-is-active {background-color: green}
-      ul {list-style: none;}
-      li {margin-bottom: 1ex;}
-      h2 {font-size: 14pt;}
-
-      .hero-unit {padding: 20px;}
-      h1 {padding-bottom: 1ex;}
-      .chzn-container {margin-top: 1px; font-size: 10pt;}
-
-      table tr td {white-space: nowrap; overflow: hidden; max-width: 500px;}
-      table tr td:first-of-type {width: 120px;}
-      table tr td:nth-of-type(2) {width: 410px;}
-    </style>
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="../assets/js/html5shiv.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-static-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="/">Translation status</a>
-          <div class="nav-collapse collapse pull-right">
-            <?php
-            $mtime = filemtime(__DIR__ . '/../data/.');
-            $delta = time() - $mtime;
-            ?>
-            <p class="navbar-text">Last update: <span class="label label-<?php echo $delta < 3600 * 24 ? 'success' : ($delta < 3600 * 24 * 3 ? 'warning' : 'error') ?>"><?php echo timeAgoInWords($mtime); ?></span></p>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-
-      <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">
-        <h1>Find what videos has recenly been translated&mdash;in a user-friendly way.</h1>
-        <form class="form-search" action="index.php">
-          <select type="text" name="language" class="input-large chzn-select" data-placeholder="Choose your language&hellip;">
-            <?php
-            foreach (scandir(__DIR__ . '/../data') as $file) {
-              if (strpos($file, 'times_') === 0) {
-                $language = substr($file, 6);
-                $forlg = ucwords(str_replace('-', ' ', $language));
-                $raw = file_get_contents(__DIR__ . "/../data/$file");
-
-                $data = [];
-                foreach (explode("\n", $raw) as $line) {
-                  if (!$line)
-                    continue;
-                  list($id) = explode(" ", $line);
-                  $data[] = $id;
-                }
-
-                $count = count(array_unique($data));
-                if ($count === 0)
-                  continue;
-                echo "<option value=\"$language\"" . (isset($_GET['language']) && $language === $_GET['language'] ? ' selected="selected"' : '') . ">$forlg ($count)</option>\n";
-              }
-            }
-            ?>
-          </select>
-          <button type="submit" class="btn btn-primary">Show content</button>
-        </form>
-      </div>
-
-      <?php
-      if (isset($_GET['language'])):
-      ?>
-        <table class="table table-striped">
-        <?php
-          $language = str_replace('.', '', str_replace('/', '', $_GET['language']));
-          $raw = file_get_contents(__DIR__ . "/../data/times_$language");
-          $data = [];
-          foreach (explode("\n", $raw) as $line) {
-            if (!$line)
-              continue;
-            list($id, $date, $time) = explode(" ", $line);
-            $data[$id] = strToTime("$date $time");
-          }
-          arsort($data);
-          $last_date = NULL;
-          foreach ($data as $youtube_id => $time) {
-            $date = date('Y-m-d', $time);
-            if ($date !== $last_date) {
-              $count = 0;
-              foreach ($data as $i => $t) {
-                if ($date === date('Y-m-d', $t))
-                  $count++;
-              }
-
-              $last_date = $date;
-              echo "</table>\n<h2>" .
-                (
-                  $date === date('Y-m-d') ?
-                    "Today" :
-                  $date === date('Y-m-d', time() - 3600 * 24) ?
-                    "Yesterday" :
-                    "$date (" . timeAgoInWords($time) . ")"
-                ) .
-                " ($count)</h2>\n<table class=\"table table-striped\">";
-            }
-            try {
-              $meta = getYoutube($youtube_id);
-              $amara = getAmara($youtube_id, $lang);
-            } catch (Exception $e) {
-              echo "<tr><td colspan=\"3\">API limit exceeded, please try later for more results</td></tr>";
-              break;
-            }
-            $uilang = 'en';
-            if (in_array($lang, ['ar', 'bg', 'cs', 'fr', 'he', 'it', 'pl', 'zh-cn', 'zh-tw'])) {
-              $uilang = $lang;
-            }
-            $amara_url = "http://www.amara.org/$uilang/videos/{$amara->video_id}/$lang/";
-            $amara_text = truncate($amara->subtitles->title, 6) ?: "amara link";
-            $youtube_text = truncate($meta->data->title, 6) ?: "khan academy link";
-            echo "<tr><td><button title=\"test\" data-clipboard-text=\"$youtube_id\"></button> <a href=\"http://www.youtube.com/watch/?v=$youtube_id\" target=\"_blank\"><code>$youtube_id</code></a></td><td><a href=\"$amara_url\" target=\"_blank\">$amara_text</a></td><td><a href=\"http://www.khanacademy.org/video?v=$youtube_id\" target=\"_blank\">$youtube_text</a></td></tr>";
-          }
-        ?>
-        </table>
-      <?php
-      endif;
-      ?>
-
-      <footer>
-        <p>&copy; <a href="http://twitter.com/mikulasdite" target="_blank">Mikuláš Dítě</a> 2013, released for Khanova škola Czech Republic <a href="https://khanovaskola.cz" target="_blank">khanovaskola.cz</a></p>
-        <p>If you have any feature request or issue to report, please contact me <a href="http://twitter.com/mikulasdite" target="_blank">@MikulasDite</a> or at <a href="mailto:mikulas@khanovaskola.cz" target="_blank">mikulas@khanovaskola.cz</a>.
-      </footer>
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!--<script src="/js/bootstrap.min.js"></script>-->
-    <script src="/js/jquery-1.9.1.min.js"></script>
-    <script src="/js/jquery-ui-1.10.3.custom.min.js"></script>
-    <script src="/js/chosen.jquery.min.js"></script>
-    <script src="/js/ZeroClipboard.min.js"></script>
-    <script src="/js/jquery.pulse.min.js"></script>
-    <script>
-      $(".chzn-select").chosen({no_results_text: "Language not supported"});
-      ZeroClipboard.setDefaults({moviePath: '/js/ZeroClipboard.swf'});
-      var clip = new ZeroClipboard($("button[data-clipboard-text]"));
-      clip.on('complete', function(client, args) {
-        var sibs = $("[data-clipboard-text=\"" + args.text + "\"]").siblings('a:first-of-type').find('code');
-        sibs.pulse({backgroundColor: 'rgb(200, 230, 170)'}, {pulses: 2, duration: 200});
-      });
-    </script>
-  </body>
-</html>
-<?php
-
-function truncate($text, $limit) {
-  $new = $text;
-  if (str_word_count($text, 0) > $limit) {
-      $words = str_word_count($text, 2);
-      $pos = array_keys($words);
-      $new = trim(mb_substr($text, 0, $pos[$limit])) . '&hellip;';
+    list($time, $youtube_id, $langs) = explode("\t", $line);
+    if (strpos($langs, $language) !== FALSE) {
+      $data[] = [$time, $youtube_id];
+    }
   }
-  return $new;
+
+  return $data;
 }
 
 function getYoutube($youtube_id)
@@ -273,4 +103,3 @@ function timeAgoInWords($time) {
     if ($delta < 1051920) return 'a year ago';
     return round($delta / 525960) . ' years ago';
 }
-?>
