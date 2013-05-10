eb7af9b415fae9281de01bc5f4182f72e218d396
Mikulas <rullaf@gmail.com>
2013-05-10 19:30:05 +0200

    update daemon to look for last location

--- daemon.php
+++ daemon.php
@@ -4,9 +4,34 @@ require __DIR__ . '/amara.php';
 
 define('DATA_FILE', __DIR__ . '/translations.dat');
 
+// read last result
+$fp = fopen(DATA_FILE, "r");
+$pos = -2; // skip first newline
+do {
+	fseek($fp, $pos, SEEK_END);
+	$char = fgetc($fp);
+	$pos--;
+} while ($char !== "\n");
+$last = explode("\t", fgets($fp))[1];
+fclose($fp);
+
+unset($fp);
+unset($pos);
+unset($char);
+
 $handle = fopen(__DIR__ . "/youtube_ids.dat", "r");
+
+// seek to last result
+if ($last) {
+	echo "Seeking last location ($last)\n";
+	while (substr(fgets($handle, 12 * 8), 0, 11) !== $last);
+}
+
+echo "Daemon started\n";
+// daemon loop
 for (;;) {
 	if (feof($handle)) {
+		file_put_contents('cycles.dat', time() . "\n", FILE_APPEND);
 		fseek($handle, 0);
 	}
 
