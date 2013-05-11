95e6dd0d6cc420316b22575ddddb1dc64d3e9157
Mikulas <rullaf@gmail.com>
2013-05-12 00:04:05 +0200

    save directly to database from daemon

--- daemon.php
+++ daemon.php
@@ -1,31 +1,18 @@
 <?php
 
 require __DIR__ . '/amara.php';
+require __DIR__ . '/vendor/autoload.php';
 
-define('DATA_FILE', __DIR__ . '/data/translations_%s.dat');
+$configurator = new Nette\Configurator;
+$configurator->enableDebugger(__DIR__ . '/log');
+$configurator->setTempDirectory(__DIR__ . '/temp');
+$configurator->addConfig(__DIR__ . '/config.neon');
+$configurator->addConfig(__DIR__ . '/config.local.neon');
+$container = $configurator->createContainer();
 
 @mkdir(__DIR__ . '/data');
 
-// read last result
-$fp = fopen(getDatafileName(), "r");
-if ($fp) {
-	$pos = -2; // skip first newline
-	do {
-		fseek($fp, $pos, SEEK_END);
-		$char = fgetc($fp);
-		$pos--;
-	} while ($char !== "\n");
-	$last = explode("\t", fgets($fp))[1];
-	fclose($fp);
-
-	unset($fp);
-	unset($pos);
-	unset($char);
-}
-
-$handle = fopen(__DIR__ . "/youtube_ids.dat", "r");
-
-// seek to last result
+$last = trim(@file_get_contents(__DIR__ . '/last_checked_youtube_id.dat')); // @ - file might not exist
 if ($last) {
 	echo "Seeking last location ($last)\n";
 	while (substr(fgets($handle, 12 * 8), 0, 11) !== $last);
@@ -36,6 +23,7 @@ echo "Daemon started\n";
 for (;;) {
 	if (feof($handle)) {
 		file_put_contents(__DIR__ . '/cycles.dat', time() . "\n", FILE_APPEND);
+		file_put_contents(__DIR__ . '/mem_usage.log', memory_get_usage() . "\n", FILE_APPEND);
 		fseek($handle, 0);
 	}
 
@@ -46,17 +34,26 @@ for (;;) {
 			$langs[] = $lang;
 		}
 	}
-	if ($langs) {
-		file_put_contents(getDatafileName(), time() . "\t$id\t;" . implode(';', $langs) . ";\n", FILE_APPEND);
+
+	foreach ($langs as $lang) {
+		if ($container->database->table('translation')->where('day', date('Y-m-d', time() - 3600 * 24))->where('youtube_id', $id)->where('language', $lang)->count() !== 0) {
+			// this video has been translated yesterday or before, do not save as new
+			continue;
+		}
+
+		try {
+			$container->database->table('translation')->insert([
+				'day' => date('Y-m-d'),
+				'youtube_id' => $id,
+				'language' => $lang,
+			]);
+		} catch (PDOException $e) {
+			if ($e->getCode() != 23000) {
+				throw $e;
+			}
+			// else duplicate entry, ignore
+		}
 	}
-	//echo "$line\n";
-}
 
-function mem() {
-	echo number_format(memory_get_usage() / 1e6) . " MB\n";
-}
-
-function getDatafileName()
-{
-	return sprintf(DATA_FILE, date('Y-m-d'));
+	file_put_contents(__DIR__ . '/last_checked_youtube_id.dat', $id);
 }
--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/TemplateRouter.php';
 $languages = require __DIR__ . '/languages.php';
 
 if (extension_loaded('newrelic')) {
-  newrelic_set_appname('khan-report.khanovaskola.cz');
+	newrelic_set_appname('khan-report.khanovaskola.cz');
 }
 
 $configurator = new Nette\Configurator;
@@ -22,152 +22,105 @@ $container->application->run();
 
 function isDaemonRunning()
 {
-  $processes = (int) trim(`ps aux | grep daemon.php | grep grep -v | wc -l`);
-  return $processes > 0;
-}
-
-function getLastTimestamp()
-{
-  // read last result
-  $fp = fopen(__DIR__ . '/../translations.dat', "r");
-  $pos = -2; // skip first newline
-  do {
-    fseek($fp, $pos, SEEK_END);
-    $char = fgetc($fp);
-    $pos--;
-  } while ($char !== "\n");
-  $time = explode("\t", fgets($fp))[0];
-  fclose($fp);
-
-  return $time;
-}
-
-function getBufferedTranslations($language)
-{
-  $handle = fopen(__DIR__ . "/../data/translations_" . date('Y-m-d') . ".dat", "r");
-  $min = strToTime(date('Y-m-d'));
-  $max = $min + 3600 * 24;
-
-  $data = [];
-  while (!feof($handle)) {
-    $line = trim(fgets($handle));
-    if (!$line)
-      continue;
-
-    list($time, $youtube_id, $langs) = explode("\t", $line);
-    // TODO old buffer replace with new faster strpos
-    if ($time >= $min && $time < $max && preg_match("~(\s|;)$language(;|$)~", $line)) {
-      if (!in_array($youtube_id, $data))
-        $data[] = $youtube_id;
-    }
-  }
-
-  return $data;
+	$processes = (int) trim(`ps aux | grep daemon.php | grep grep -v | wc -l`);
+	return $processes > 0;
 }
 
 function getDays($language)
 {
-  $data = [];
-  $today = date('Y-m-d');
-  $yesterday = date('Y-m-d', time() - 3600 * 24);
-  foreach (getTranslations($language) as $row) {
-    if (!isset($data[$row->day])) {
-      $data[$row->day] = [];
-    }
-    $data[$row->day][] = $row->youtube_id;
-  }
+	$data = [];
+	foreach (getTranslations($language) as $row) {
+		if (!isset($data[$row->day])) {
+			$data[$row->day] = [];
+		}
+		$data[$row->day][] = $row->youtube_id;
+	}
 
-  $data_today = [];
-  foreach (getBufferedTranslations($language) as $node) {
-    if (!in_array($node, $data[$yesterday]))
-      $data_today[] = $node;
-  }
-
-  return array_merge([$today => $data_today], $data);
+	return $data;
 }
 
 function getTranslations($language)
 {
-  global $container;
-  return $container->database->table('translation')->select('youtube_id, day')->where('language', $language)->order('day DESC, id DESC');
+	global $container;
+	return $container->database->table('translation')->select('youtube_id, day')->where('language', $language)->order('day DESC, id DESC');
 }
 
 function getLanguageCounts()
 {
-  global $container;
-  return $container->database->table('translation')->select('Count(DISTINCT youtube_id) AS count, language')->group('language')->fetchPairs('language', 'count');
+	global $container;
+	return $container->database->table('translation')->select('Count(DISTINCT youtube_id) AS count, language')->group('language')->fetchPairs('language', 'count');
 }
 
 function getLanguageCount($language)
 {
-  global $container;
-  return $container->database->table('translation')->select('DISTINCT youtube_id')->where('language', $language)->count();
+	global $container;
+	return $container->database->table('translation')->select('DISTINCT youtube_id')->where('language', $language)->count();
 }
 
 function getYoutube($youtube_id)
 {
-  @mkdir(__DIR__ . '/cache'); // @ - may already exist
-  $cache = __DIR__ . "/cache/youtube_" . str_replace('=', '', base64_encode($youtube_id));
-  $meta = NULL;
-  if (file_exists($cache)) {
-    $meta = unserialize(file_get_contents($cache));
-  }
-  if (!$meta) {
-    $meta = json_decode(@file_get_contents("http://gdata.youtube.com/feeds/api/videos/$youtube_id?v=2&alt=jsonc&prettyprint=false"));
-    if (!$meta) {
-      throw new Exception;
-    }
-    file_put_contents($cache, serialize($meta));
-  }
-  return $meta;
+	@mkdir(__DIR__ . '/cache'); // @ - may already exist
+	$cache = __DIR__ . "/cache/youtube_" . str_replace('=', '', base64_encode($youtube_id));
+	$meta = NULL;
+	if (file_exists($cache)) {
+		$meta = unserialize(file_get_contents($cache));
+	}
+	if (!$meta) {
+		$meta = json_decode(@file_get_contents("http://gdata.youtube.com/feeds/api/videos/$youtube_id?v=2&alt=jsonc&prettyprint=false"));
+		if (!$meta) {
+			throw new Exception;
+		}
+		file_put_contents($cache, serialize($meta));
+	}
+	return $meta;
 }
 
 
 function getAmara($youtube_id, $lang) {
-  $cache = __DIR__ . "/cache/amara_{$lang}_" . str_replace('=', '', base64_encode($youtube_id));
-  $meta = NULL;
+	$cache = __DIR__ . "/cache/amara_{$lang}_" . str_replace('=', '', base64_encode($youtube_id));
+	$meta = NULL;
 
-  if (file_exists($cache)) {
-    $meta = unserialize(file_get_contents($cache));
-    return $meta;
-  }
-  if (!$meta) {
-    $url = 'http://www.universalsubtitles.org/widget/rpc/jsonp/show_widget?video_url=' . urlencode("\"http://www.youtube.com/watch?v={$youtube_id}\"") . '&is_remote=true&base_state=%7B%22language%22%3A%22' . $lang .'%22%7D&callback=';
-    $res = "[" . substr(@file_get_contents($url), 1, -2) . "]"; // remove colon and parenthesis
-    $meta = json_decode($res);
-    $id = NULL;
-    if (!$meta) {
-      throw new Exception;
-    }
-    $meta = $meta[0];
-    file_put_contents($cache, serialize($meta));
-    return $meta;
-  }
+	if (file_exists($cache)) {
+		$meta = unserialize(file_get_contents($cache));
+		return $meta;
+	}
+	if (!$meta) {
+		$url = 'http://www.universalsubtitles.org/widget/rpc/jsonp/show_widget?video_url=' . urlencode("\"http://www.youtube.com/watch?v={$youtube_id}\"") . '&is_remote=true&base_state=%7B%22language%22%3A%22' . $lang .'%22%7D&callback=';
+		$res = "[" . substr(@file_get_contents($url), 1, -2) . "]"; // remove colon and parenthesis
+		$meta = json_decode($res);
+		$id = NULL;
+		if (!$meta) {
+			throw new Exception;
+		}
+		$meta = $meta[0];
+		file_put_contents($cache, serialize($meta));
+		return $meta;
+	}
 }
 
 function timeAgoInWords($time) {
-    if (!$time) {
-        return FALSE;
-    } elseif (is_numeric($time)) {
-        $time = (int) $time;
-    } elseif ($time instanceof DateTime) {
-        $time = $time->format('U');
-    } else {
-        $time = strtotime($time);
-    }
+	if (!$time) {
+		return FALSE;
+	} elseif (is_numeric($time)) {
+		$time = (int) $time;
+	} elseif ($time instanceof DateTime) {
+		$time = $time->format('U');
+	} else {
+		$time = strtotime($time);
+	}
 
-    $delta = time() - $time;
+	$delta = time() - $time;
 
-    $delta = round($delta / 60);
-    if ($delta == 0) return 'moments ago';
-    if ($delta == 1) return 'a minute ago';
-    if ($delta < 45) return "$delta minutes ago";
-    if ($delta < 90) return 'an hour ago';
-    if ($delta < 1440) return round($delta / 60) . ' hours ago';
-    if ($delta < 2880) return 'yesterday';
-    if ($delta < 43200) return round($delta / 1440) . ' days ago';
-    if ($delta < 86400) return 'a month ago';
-    if ($delta < 525960) return round($delta / 43200) . ' months ago';
-    if ($delta < 1051920) return 'a year ago';
-    return round($delta / 525960) . ' years ago';
+	$delta = round($delta / 60);
+	if ($delta == 0) return 'moments ago';
+	if ($delta == 1) return 'a minute ago';
+	if ($delta < 45) return "$delta minutes ago";
+	if ($delta < 90) return 'an hour ago';
+	if ($delta < 1440) return round($delta / 60) . ' hours ago';
+	if ($delta < 2880) return 'yesterday';
+	if ($delta < 43200) return round($delta / 1440) . ' days ago';
+	if ($delta < 86400) return 'a month ago';
+	if ($delta < 525960) return round($delta / 43200) . ' months ago';
+	if ($delta < 1051920) return 'a year ago';
+	return round($delta / 525960) . ' years ago';
 }
