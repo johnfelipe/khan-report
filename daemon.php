218691af652b52b0444c86c9ff01798a276d4fb6
Deploybot <deploybot@localhost>
2013-05-12 00:48:54 +0200

    debug

--- daemon.php
+++ daemon.php
@@ -4,7 +4,7 @@ require __DIR__ . '/amara.php';
 require __DIR__ . '/vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
-$configurator->enableDebugger(__DIR__ . '/log');
+$configurator->setDebugMode(TRUE);
 $configurator->setTempDirectory(__DIR__ . '/temp');
 $configurator->addConfig(__DIR__ . '/config.neon');
 $configurator->addConfig(__DIR__ . '/config.local.neon');
